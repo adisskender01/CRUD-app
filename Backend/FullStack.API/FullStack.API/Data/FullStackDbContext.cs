28e65708972b431f948874f3807c3b2f73fc9c52
adisskender01 <112349156+adisskender01@users.noreply.github.com>
2023-09-22 22:18:18 +0200

    Add files via upload

---
+++ Backend/FullStack.API/FullStack.API/Data/FullStackDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FullStack.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.API.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
